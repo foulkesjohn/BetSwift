d38c27c2997ac46c19c50d5b77ac6e0a2d6e4436
John Foulkes <foulkes_john@hotmail.com>
2018-04-03 19:39:53 +0100

    Removed unused code

--- Sources/BetSwift/Betting.swift
+++ Sources/BetSwift/Betting.swift
@@ -15,33 +15,3 @@ public extension EventLoop {
   }
   
 }
-
-//public struct Betting {
-//
-//  public enum Error: Swift.Error {
-//    case encodingFailed
-//  }
-//
-//  private let group: MultiThreadedEventLoopGroup
-//
-//  private var loop: EventLoop {
-//    return group.next()
-//  }
-//
-//  public init(group: MultiThreadedEventLoopGroup = MultiThreadedEventLoopGroup(numThreads: 1)) {
-//    self.group = group
-//  }
-//
-//  public func place(instructions: [PlaceInstruction]) -> EventLoopFuture<PlaceExecutionReport> {
-//    let promise: EventLoopPromise<PlaceExecutionReport> = loop.newPromise()
-//    guard let data = try? JSONEncoder().encode(instructions) else {
-//      promise.fail(error: Error.encodingFailed)
-//    }
-//    let url = Endpoint.Betting.placeOrders(instructions).url
-//    let resource = Resource<PlaceExecutionReport>(url: url,
-//                                                  method: HttpMethod.post(data))
-//    return Webservice.shared.load(resource)
-//  }
-//
-//}
-
